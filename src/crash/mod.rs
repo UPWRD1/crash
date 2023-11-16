40d20631e3bef85050ad4f33ed20bf661645d929
Luke Davis <luke.n.davis.7@gmail.com>
2023-11-16 12:00:13 -0600

    Refactoring

--- src/crash/mod.rs
+++ src/crash/mod.rs
@@ -1,14 +1,16 @@
 #[allow(dead_code)]
-pub struct Shade {
+pub struct K0 {
     pub msg: String,
     pub digest: String,
 }
 
 #[allow(dead_code)]
-impl Shade {
-    pub fn hash(input: &str, dlen: u32) -> String {
+impl K0 {
+    //eea433b148525d60cf756a0c3dee8faac7c2e6cb0578913166369ec0ce4b1cde
+    #[inline]
+    pub fn hash(input: &str) -> String {
         // Initialization
-        let word_size = 16;
+        const WORDSIZE: usize = 16; //16
 
         // Padding
         let mut msg_bytes: Vec<u8> = input.bytes().collect();
@@ -23,12 +25,145 @@ impl Shade {
         }
 
         // Create words
-        let stage1_word_chunks: Vec<_> = msg_bytes.chunks(word_size).collect();
-        let mut stage2_words: Vec<u64> = vec![];
-        let stage1_words: Vec<&u8> = stage1_word_chunks.into_iter().flatten().collect();
+        let stage1_words: Vec<&u8> = msg_bytes
+            .chunks(WORDSIZE)
+            .collect::<Vec<&[u8]>>()
+            .into_iter()
+            .flatten()
+            .collect::<Vec<&u8>>();
+        let sum: u32 = stage1_words.iter().map(|x| **x as u32).sum();
+
         // Operations
-        for (count, stage1_word) in stage1_words.clone().into_iter().enumerate() {
-            let sum: u32 = stage1_words.iter().map(|x| **x as u32).sum();
+        let mut accumulator: String = "".to_string();
+        let _: Vec<u64> = stage1_words
+            .clone()
+            .into_iter()
+            .enumerate()
+            .map(|(count, stage1_word)| {
+                let mut curr_byte: u64 = *stage1_word as u64;
+                let mut next_byte: u64 = **stage1_words.iter().next().unwrap() as u64;
+                // Interlacing
+                curr_byte *= (curr_byte | (curr_byte.rotate_right(8))) & 0x00FF00FF;
+                curr_byte += (curr_byte | (curr_byte.rotate_left(next_byte as u32).reverse_bits()))
+                    & 0x0F0F0F0F;
+                curr_byte ^=
+                    (sum as u64 | (curr_byte.wrapping_shl(2 * curr_byte as u32))) & 0x33333333;
+                curr_byte *= curr_byte.wrapping_add((sum as u64 | (curr_byte >> 1)) & 0x55555555);
+
+                next_byte ^= (next_byte | (next_byte.rotate_left(8))) & 0x00FF00FF;
+                next_byte ^=
+                    (next_byte | (next_byte.wrapping_shr(4 * next_byte as u32))) & 0x0F0F0F0F;
+                next_byte += (next_byte | (next_byte.rotate_right(2))) & 0x33333333;
+                next_byte += (next_byte | (next_byte >> 1)) & 0x55555555;
+
+                let mut interlaced = curr_byte
+                    | (next_byte).wrapping_mul(next_byte.wrapping_add(WORDSIZE as u64))
+                        ^ (sum as u64 & count as u64).reverse_bits();
+
+                interlaced ^= interlaced.wrapping_shr(WORDSIZE.wrapping_sub(256) as u32);
+                interlaced = (85331_u64.wrapping_mul(!interlaced))
+                    .wrapping_shr((WORDSIZE.wrapping_sub(16) as u32).reverse_bits());
+                accumulator = format!("{accumulator}{:x}", interlaced);
+                interlaced
+            })
+            .collect::<Vec<u64>>();
+
+        let sum_acc: u32 = accumulator
+            .chars()
+            .collect::<Vec<char>>()
+            .iter()
+            .map(|x| *x as u32)
+            .sum::<u32>();
+        let mut truncated: Vec<char>;
+        let dlen: usize = 256 as usize;
+        let dlen2: usize = (256 * 2) as usize;
+        let dlen3: usize = (256 * 3) as usize;
+
+        if sum_acc & 1 == 0 {
+            truncated = accumulator[dlen as usize..dlen2]
+                .to_string()
+                .chars()
+                .collect::<Vec<char>>()
+                .to_owned();
+        } else {
+            truncated = accumulator[dlen2..dlen3]
+                .to_string()
+                .chars()
+                .collect::<Vec<char>>()
+                .to_owned();
+        }
+        truncated.rotate_right(sum_acc as usize & 127);
+        truncated.truncate(64);
+        truncated
+            .chunks(8)
+            .map(|c| c.iter().collect::<String>())
+            .collect::<Vec<String>>()
+            .join("")
+    }
+    
+    //51593a79fffffdb30e2e82e7fffff451ec7f7a45fffffc8e1a7eec2efff67e4c
+    #[inline]
+    pub fn hash1(input: &str) -> String {
+        // Initialization
+        const WORDSIZE: usize = 16; //16
+        const DLENX1: usize = 256 as usize;
+        const DLENX2: usize = (DLENX1 * 2) as usize;
+        const DLENX3: usize = (DLENX1 * 3) as usize;
+
+        // Padding
+        let mut msg_bytes: Vec<u8> = input.bytes().collect();
+        let mut count1: usize = 0;
+        if msg_bytes.len() < 512 {
+            while msg_bytes.len() < 512 {
+                msg_bytes.push(msg_bytes[count1].wrapping_add(17 ^ count1 as u8));
+                //msg_bytes.push(0);
+                count1 += 1;
+            }
+        }
+
+        // Create words
+        let stage1_words: Vec<&u8> = msg_bytes
+            .chunks(WORDSIZE)
+            .collect::<Vec<&[u8]>>()
+            .into_iter()
+            .flatten()
+            .collect::<Vec<&u8>>();
+        let sum: u32 = stage1_words.iter().map(|x| **x as u32).sum();
+
+        // Operations
+        let mut accumulator: String = "".to_string();
+        let mut truncated: Vec<char>;
+        if sum & 1 == 0 {
+            truncated = process(&stage1_words, sum, &mut accumulator, WORDSIZE)
+                [DLENX1 as usize..DLENX2]
+                .to_string()
+                .chars()
+                .collect::<Vec<char>>()
+                .to_owned();
+        } else {
+            truncated = process(&stage1_words, sum, &mut accumulator, WORDSIZE)[DLENX2..DLENX3]
+                .to_string()
+                .chars()
+                .collect::<Vec<char>>()
+                .to_owned();
+        }
+        truncated.rotate_right(sum as usize & 127);
+        truncated.truncate(WORDSIZE * 4);
+        truncated
+            .chunks(8)
+            .map(|c| c.iter().collect::<String>())
+            .collect::<Vec<String>>()
+            .join("")
+    }
+}
+
+#[inline]
+fn process(stage1_words: &Vec<&u8>, sum: u32, accumulator: &mut String, wordsize: usize) -> String {
+    stage1_words
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(move |(count, stage1_word)| {
             let mut curr_byte: u64 = *stage1_word as u64;
             let mut next_byte: u64 = **stage1_words.iter().next().unwrap() as u64;
             // Interlacing
@@ -44,50 +179,15 @@ impl Shade {
             next_byte += (next_byte | (next_byte >> 1)) & 0x55555555;
 
             let mut interlaced = curr_byte
-                | (next_byte).wrapping_mul(next_byte.wrapping_add(word_size as u64))
+                | (next_byte).wrapping_mul(next_byte.wrapping_add(wordsize as u64))
                     ^ (sum as u64 & count as u64).reverse_bits();
 
-            interlaced ^= interlaced.wrapping_shr(word_size.wrapping_sub(256) as u32);
-            interlaced = (85331_u64.wrapping_mul(interlaced))
-                .wrapping_shr(word_size.wrapping_sub(16) as u32);
-            stage2_words.push(interlaced);
-        }
-
-        // Truncate and formatting
-        let mut accumulator: String = "".to_string();
-        for item in stage1_words {
-            accumulator = format!("{accumulator}{:x}", item);
-        }
-
-        let sum: u32 = accumulator
-            .chars()
-            .collect::<Vec<char>>()
-            .iter()
-            .map(|x| *x as u32)
-            .sum::<u32>();
-        let mut truncated: Vec<char>;
-        let dlen2: usize = (dlen * 2) as usize;
-        let dlen3: usize = (dlen * 3) as usize;
-
-        if sum & 1 == 0 {
-            truncated = accumulator[dlen as usize..dlen2]
-                .to_string()
-                .chars()
-                .collect::<Vec<char>>()
-                .to_owned();
-        } else {
-            truncated = accumulator[dlen2..dlen3]
-                .to_string()
-                .chars()
-                .collect::<Vec<char>>()
-                .to_owned();
-        }
-        truncated.rotate_right(sum as usize & 127);
-        truncated.truncate(64);
-        truncated
-            .chunks(8)
-            .map(|c| c.iter().collect::<String>())
-            .collect::<Vec<String>>()
-            .join("")
-    }
+            interlaced ^= interlaced.wrapping_shr(wordsize.wrapping_sub(256) as u32);
+            interlaced = (85331_u64.wrapping_mul(!interlaced))
+                .wrapping_shr((wordsize.wrapping_sub(16) as u32).reverse_bits());
+            *accumulator = format!("{accumulator}{:x}", interlaced);
+            let res = accumulator.to_owned();
+            res
+        })
+        .collect::<String>()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,13 @@ pub fn increment_string(s: &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crash::Shade;
+    use crash::K0;
     #[test]
     fn collision_test() {
-        let inputs = vec!["input1", "jnput", "knput"];
+        let inputs = vec!["input", "jnput", "knput"];
         let mut hash_set = std::collections::HashSet::new();
         for input in inputs {
-            let hash_value = Shade::hash(input, 256);
+            let hash_value = K0::hash(input);
             assert!(
                 !hash_set.contains(&hash_value),
                 "Collision detected for input: {}",
@@ -43,8 +43,8 @@ mod tests {
 
     #[test]
     fn security_test() {
-        let hash_value1 = Shade::hash("test_input", 256);
-        let hash_value2 = Shade::hash("test_input", 256);
+        let hash_value1 = K0::hash("test_input");
+        let hash_value2 = K0::hash("test_input");
         println!("{hash_value1}\n\n{hash_value2}");
         assert_eq!(
             hash_value1, hash_value2,
@@ -57,9 +57,11 @@ mod tests {
         let num_inputs = 1000;
         let mut hash_count = std::collections::HashMap::new();
 
+        let start_time = std::time::Instant::now();
+
         for i in 0_i32..num_inputs {
             let input = format!("input{}", i);
-            let hash_value = Shade::hash(&input, 256);
+            let hash_value = K0::hash(&input);
 
             *hash_count.entry(hash_value).or_insert(0) += 1;
         }
@@ -67,22 +69,25 @@ mod tests {
         // Check that no hash value has a significantly higher count than others.
         let max_count = *hash_count.values().max().unwrap();
         let min_count = *hash_count.values().min().unwrap();
+        let elapsed_time = start_time.elapsed();
+        println!("Hashed {} inputs in {:?}", num_inputs, elapsed_time);
         assert!(
-            max_count - min_count <= 2
+            max_count - min_count <= 1
             ,
             "Hash values are not evenly distributed!\n {max_count}\n\n {min_count}"
         );
+        println!("Difference: {} ({}), ({})", max_count - min_count, max_count, min_count)
     }
 
     #[test]
     fn performance_test() {
-        let num_inputs = 10000;
+        let num_inputs = 1000;
         let input = "test";
 
         let start_time = std::time::Instant::now();
 
         for _ in 0..num_inputs {
-            let _ = Shade::hash(input, 256);
+            let _ = K0::hash(input);
         }
 
         let elapsed_time = start_time.elapsed();
@@ -96,7 +101,7 @@ mod tests {
         let mut previous: Vec<String> = vec![];
         let mut current = start.clone();
         while current <= end {
-            let z = Shade::hash(&current,256);
+            let z = K0::hash(&current);
             println!("{current}\t-\t{z}");
             for (index, _item) in previous.iter().enumerate() {
                 assert_ne!(z, previous[index])
@@ -111,31 +116,47 @@ mod tests {
 
     #[test]
     fn qbf() {
-            let x = Shade::hash("The quick brown fox jumps over the lazy dog".into(), 256); 
+            let x = K0::hash("The quick brown fox jumps over the lazy dog".into()); 
             println!("{x}\n");
-            let y = Shade::hash("The quick brown fox jumps over the ØÂôò@ÈÞ".into(), 256); 
+            let y = K0::hash("The quick brown fox jumps over the ØÂôò@ÈÞ".into()); 
             println!("{y}\n");
             assert_ne!(x, y);
     }
 
     #[test]
     fn empty() {
-        let res = Shade::hash("", 256);
+        let res = K0::hash("");
         println!("{res}");
     }
 
     #[test]
     fn dump() {
         let start = String::from("a");
-        let end = String::from("zz");
+        let end = String::from("z");
         let mut current = start.clone();
         while current <= end {
-            let z = Shade::hash(&current,256);
+            let z = K0::hash(&current);
             println!("{z}");
             current = increment_string(&current);
             //let ten_millis = std::time::Duration::from_millis(50);
 
             //std::thread::sleep(ten_millis);
         }
+    }
+
+    #[test]
+    fn dogfood() {
+        let hashres = K0::hash("This is the starting hash. This is a long sentance that will be dogfooded back into the hash algorithm.");
+        let hashres2 = K0::hash(&hashres);
+        println!("{hashres}\n\n{hashres2}");
+        assert_ne!(hashres, hashres2);
+    }
+
+    #[test]
+    fn len_ext() {
+        let hashres = K0::hash("count=10&lat=37.351&user_id=1&long=-119.827&waffle=eggo");
+        let hashres2 = K0::hash("count=10&lat=37.351&user_id=1&long=-119.827&waffle=eggo\x7f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x28\x28&waffle=liege");
+        println!("{hashres}\n\n{hashres2}");
+        assert_ne!(hashres, hashres2);
     }
 }
